5d9c2edd74af4c4c152d649c70edbaa819fd327f
Vladimir Likic <v.likic1@gmail.com>
2021-12-20 07:37:42 +1100

    Updated 'genpdf.sh'

--- genpdf.sh
+++ genpdf.sh
@@ -3,4 +3,4 @@
 # Should be called from the project root folder.
 
 OUTFILE=test_cases.md
-pandoc $OUTFILE --to pdf --number-sections > ${OUTFILE%.*}.pdf
+pandoc $OUTFILE --number-sections -o ${OUTFILE%.*}.pdf
